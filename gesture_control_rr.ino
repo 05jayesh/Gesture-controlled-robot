1c168d6313ec06a07104ab03f15c0f03b4fc758a
Jayesh Prakash <41137643+05jayesh@users.noreply.github.com>
2021-03-25 00:20:37 +0530

    Add files via upload

---
+++ gesture_control_rr.ino
@@ -0,0 +1,117 @@
+// Basic Bluetooth sketch HC-05_02
+// Connect the HC-05 module and communicate using the serial monitor
+//
+// The HC-05 defaults to commincation mode when first powered on.
+// The default baud rate for communication mode is 9600
+//
+ 
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(2, 3); // RX | TX
+// Connect the HC-05 TX to Arduino pin 2 RX. 
+// Connect the HC-05 RX to Arduino pin 3 TX through a voltage divider.
+// 
+ 
+char c = ' ';
+
+#include<AFMotor.h>
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+ 
+void setup() 
+{
+    Serial.begin(9600);
+    Serial.println("Arduino is ready");
+ 
+    // HC-05 default serial speed for commincation mode is 9600
+    BTserial.begin(9600);  
+
+    motor1.setSpeed(255);
+    motor2.setSpeed(255);
+}
+ 
+void loop()
+{
+ 
+    // Keep reading from HC-05 and send to Arduino Serial Monitor
+    if (BTserial.available())
+    {  
+        c = BTserial.read();
+        Serial.write(c);
+    
+ 
+    // Keep reading from Arduino Serial Monitor and send to HC-05
+   // if (Serial.available())
+    //{
+      //  c =  Serial.read();
+       // BTserial.write(c);  
+    //}
+
+     switch(c)
+{
+case 'w':
+motor1.run(FORWARD);
+motor2.run(FORWARD);
+
+break;
+
+
+case 'a':
+motor1.run(BACKWARD);
+motor2.run(FORWARD);
+
+break;
+
+
+case 's':
+motor1.run(BACKWARD);
+motor2.run(BACKWARD);
+
+break;
+
+
+case 'd':
+motor1.run(FORWARD);
+motor2.run(BACKWARD);
+
+break;
+
+
+case 't':
+motor1.run(RELEASE);
+motor2.run(FORWARD);
+
+break;
+
+
+case 'y':
+motor1.run(FORWARD);
+motor2.run(RELEASE);
+
+break;
+
+
+case 'g':
+motor1.run(RELEASE);
+motor2.run(BACKWARD);
+
+
+break;
+
+
+case 'h':
+motor1.run(BACKWARD);
+motor2.run(RELEASE);
+
+break;
+
+
+  
+}
+    }
+    else
+    {
+      motor1.run(RELEASE);
+motor2.run(RELEASE);
+    }
+ 
+}
---
+++ gesture_control_tt.ino
@@ -0,0 +1,148 @@
+#include<SPI.h>
+
+#include"Wire.h"
+
+
+
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(2, 3); // RX | TX
+// Connect the HC-05 TX to Arduino pin 2 RX. 
+// Connect the HC-05 RX to Arduino pin 3 TX through a voltage divider.
+// 
+
+
+
+
+
+
+#include "Wire.h" 
+
+
+const int MPU_ADDR = 0x68;
+
+int16_t accelerometer_x, accelerometer_y, accelerometer_z;
+int aX,aY,aZ;
+
+
+char tmp_str[7];
+
+char* convert_int16_to_str(int16_t i)
+{ 
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+}
+
+void setup() {
+  
+  Serial.begin(9600);
+  BTserial.begin(9600);
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR); 
+  Wire.write(0x6B); 
+  Wire.write(0); 
+  Wire.endTransmission(true);
+
+
+
+
+}
+void loop() {
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B);
+  Wire.endTransmission(false); 
+  Wire.requestFrom(MPU_ADDR, 7*2, true); 
+  
+
+  accelerometer_y = Wire.read()<<8 | Wire.read(); 
+  accelerometer_x = Wire.read()<<8 | Wire.read(); 
+  accelerometer_z = Wire.read()<<8 | Wire.read(); 
+
+
+
+
+  aX = (accelerometer_x / 16384.0)*90;
+  aY = (accelerometer_y / 16384.0)*90; 
+  aZ = (accelerometer_z / 16384.0)*90;
+
+
+
+  aX=aX+180;
+  aY=aY+180;
+
+    if(aX<0 && aZ<0)
+  {
+    aX=-180-aX;
+  }
+    if(aX>0 && aZ<0)
+  {
+    aX=180-aX;
+  }
+  if(aY<0 && aZ<0)
+  {
+    aY=-180-aY;
+  }
+    if(aY>0 && aZ<0)
+  {
+    aY=180-aY;
+  }
+  
+
+
+
+
+
+
+
+  Serial.print("|##| position(deg) | aX = "); Serial.print(aX);
+  Serial.print(" | aY = "); Serial.print(aY);
+
+
+
+
+  
+  Serial.println();
+
+
+  // delay
+  delay(5);
+
+if(aX<-50 && aY<-40)
+{
+BTserial.println("t");
+}
+if(aX>40 && aY<-40)
+{
+BTserial.println("g");
+}
+if(aX>40 && aY>60)
+{
+BTserial.println("h");
+}
+if(aX<-50 && aY>60)
+{
+BTserial.println("y");
+}
+
+
+
+if(aX<-50)
+{
+BTserial.println("w");
+}
+if(aY<-40)
+{
+BTserial.println("a");
+}
+if(aX>40)
+{
+BTserial.println("s");
+}
+if(aY>60)
+{
+BTserial.println("d");
+}
+
+
+
+
+}
